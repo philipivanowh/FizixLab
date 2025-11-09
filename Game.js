271177ced4e3dec394e4e45380713c8267237bc2
PhilipHersin <hahoanglan0924@gmail.com>
2025-11-08 21:42:27 -0500

    remvoe ball intial setup

--- Game.js
+++ Game.js
@@ -27,22 +27,6 @@ var groundVel = new Vec2(0,0);
 var groundAcc = new Vec2(0,0);
 var ground = new Box(groundPos, groundVel,groundAcc ,1600, 50, [255, 255, 255, 1], 1, bodyType.STATIC);
 
-
-// var ball1Pos = new Vec2(200, 100);
-// var ball1Vel = new Vec2(200,0);
-// var ball1Acc = new Vec2(0,0);
-// var ball1 = new Ball(ball1Pos,ball1Vel,ball1Acc,20,[255, 200, 20, 1],1,bodyType.DYNAMIC);
-
-// var ball2Pos = new Vec2(400, 150);
-// var ball2Vel = new Vec2(100,0);
-// var ball2Acc = new Vec2(0,0);
-// var ball2 = new Ball(ball2Pos,ball2Vel,ball2Acc,50,[100, 200, 20, 1],1,bodyType.DYNAMIC);
-
-
-// var ball3Pos = new Vec2(1000, 150);
-// var ball3Vel = new Vec2(-500,0);
-// var ball3Acc = new Vec2(0,0);
-// var ball3 = new Ball(ball3Pos,ball3Vel,ball3Acc,70,[100, 200, 20, 1],1,bodyType.DYNAMIC);
 scene.add(box);
 scene.add(box2);
 scene.add(ground);
